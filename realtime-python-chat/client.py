2be00f286d455b695cad5caad0f7f3fec4c858da
Ticoo <JLRGeorge.Lo@gmail.com>
2024-09-10 09:34:02 +0100

    Simple chat through localhost

--- realtime-python-chat/client.py
+++ realtime-python-chat/client.py
@@ -1,8 +1,45 @@
 import socket
 import threading
 
-def main():
-    pass
 
-if __name__ == '__name__':
+HOST = '127.0.0.1'
+PORT = 1234
+
+def listen_for_messages_from_server(client):
+    while 1:
+        message = client.recv(2048).decode('utf-8')
+        username = message.split('¬')[0]
+        content = message.split('¬')[1]
+
+        print(f'[{username}] {content}')
+
+
+def send_message_to_server(client):
+    while 1:
+        message = input(("Message: "))
+        client.sendall(message.encode())
+
+def communicate_to_server(client):
+
+    username = input("Enter username: ")
+
+    client.sendall(username.encode())
+
+    threading.Thread(target = listen_for_messages_from_server, args=(client, )).start()
+
+    send_message_to_server(client)
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((HOST,PORT))
+        print("successfully connected")
+    except:
+        print(f'Unable to connect to server {HOST}')
+    
+    communicate_to_server(client)
+
+if __name__ == '__main__':
+    print("hi")
     main()
--- realtime-python-chat/server.py
+++ realtime-python-chat/server.py
@@ -2,21 +2,64 @@ import socket
 import threading
 
 HOST = '127.0.0.1'
-PORT = '1234' # use any port between 0 to 65535
+PORT = 1234 # use any port between 0 to 65535
 LISTENER_LIMIT = 5 # amount of connections
 
+active_clients = [] #list of all connected users
+
+#function is threaded and listens to upcoming messages from a client
+def listen_for_messages(client, username):
+
+    while 1:
+        response = client.recv(2048).decode('utf-8')
+
+        final_msg = username + '¬' + response
+        send_messages_to_all(final_msg)
+
+
+def send_message_to_client(client, message):
+
+    client.sendall(message.encode())
+
+def send_messages_to_all(message):
+    print(active_clients)
+    for user in active_clients:
+
+        send_message_to_client(user[1], message)
+
+#function to handle client
+
+def client_handler(client):
+    #server will listen for client message that will contain username
+    
+
+    while 1:
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+            break
+        else:    
+            print("Client username is empty")
+
+    threading.Thread(target = listen_for_messages, args=(client, username, )).start()
+
+    
+
 def main():
-    server = socket.socket(socket.AF.INET, socket.SOCK_STREAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         server.bind((HOST, PORT))
+        print(f'Running the server on {HOST}')
     except:
-        raise(f'Unable to bind to host {HOST} and port {PORT}')
+        print(f'Unable to bind to host {HOST} and port {PORT}')
 
     server.listen(LISTENER_LIMIT)
 
     while 1:
         client, address = server.accept() # client represents socket of client
+
+        threading.Thread(target=client_handler, args=(client, )).start()
 
 if __name__ == '__main__':
     main()
